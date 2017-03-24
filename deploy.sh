f5e2d84468e694105161f4dfb0eb3907b1cad3fb
zizon <tngurdl00@hanmail.net>
2017-03-24 22:44:29 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,10 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 
 cd $OUTPUT_DIR
-
-ls
-
 git add -A
 git commit -m "Deploy to GitHub Pages: ${SHA} at $(date -u +'%Y-%m-%d %H:%M:%S %Z')"
 
 echo "PUSH -F"
 
 # Push it
-git push -f $SSH_REPO $TARGET_BRANCH
+git push -f https://github.com/hrmrzizon/hrmrzizon.github.com.git $TARGET_BRANCH
