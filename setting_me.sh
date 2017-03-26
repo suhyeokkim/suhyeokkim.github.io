ab17351303e63125df54f7b433b3ddd505a5a69a
zizon <tngurdl00@hanmail.net>
2017-03-26 05:09:51 +0000

    Updated to 3fe27af at 2017-03-26 05:09:51 UTC

--- setting_me.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-echo "setting_me.sh"
-
-git config --global user.name "$(git --no-pager show --no-patch --format='%an')"
-git config --global user.email "$(git --no-pager show --no-patch --format='%ae')"
-
-echo SSH_REPO
