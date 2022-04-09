ffdf7ea12866ed6a237622393caaa2c077d5017e
suhyeokkim <hermaeusmorarb@gmail.com>
2022-04-09 20:08:34 +0900

    local update at 2022-04-09 11:08:34 UTC

--- build_script_local.sh
+++ build_script_local.sh
@@ -2,7 +2,7 @@
 rm -r -f _site
 
 # setting site info for git setting
-git clone -b master --depth 1 "https://github.com/suhyeokkim/suhyeokkim.github.io.git" _site
+git clone -b master --depth 1 "$(git remote get-url origin)" _site
 
 # clean up prev repo data
 rm -r _site/*
@@ -13,7 +13,11 @@ bundle exec jekyll build
 # move to _site & commit/push
 cd _site
 git add -A
-git commit -m "Updated to $(git rev-parse --short local_update) at $(date -u +'%Y-%m-%d %H:%M:%S %Z')"
-git push -f "https://github.com/suhyeokkim/suhyeokkim.github.io.git" master
+git commit -m "local update at $(date -u +'%Y-%m-%d %H:%M:%S %Z')"
+git push origin master
 
+# remove git
+rm -r -f ./.git
+
+# recover origin path
 cd ../
