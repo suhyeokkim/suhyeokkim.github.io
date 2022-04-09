a3943f0f2499b0c1e9c838bfa957fca320eef086
suhyeokkim <hermaeusmorarb@gmail.com>
2022-04-09 19:47:38 +0900

    Updated to  at 2022-04-09 10:47:37 UTC

---
+++ build_script_local.sh
@@ -0,0 +1,16 @@
+# setting site info for git setting
+git clone -b master --depth 1 "https://github.com/suhyeokkim/suhyeokkim.github.io.git" _site
+
+# clean up data
+rm -r _site/*
+
+# build in "_site" directory
+bundle exec jekyll build
+
+# move to _site & commit/push
+cd _site
+git add -A
+git commit -m "Updated to $(git rev-parse --short local_update) at $(date -u +'%Y-%m-%d %H:%M:%S %Z')"
+git push -f "https://github.com/suhyeokkim/suhyeokkim.github.io.git" master
+
+cd ../
